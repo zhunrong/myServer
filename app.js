138f03a564d1331e4e34bbab50fd94fd5ac8b8ab
zhunrong <chenzr@chd.edu.cn>
2018-02-23 17:55:04 +0800

    'mysql'

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ const app = express();
 /*route*/
 const router = require('./routes/index');
 
+
+//中间件
+const bodyParser = require('body-parser');
+// const multer = require('multer');
+
+//静态资源
 app.use(express.static('public'));
 app.use('/html', express.static('html'));
 
 
+
+//使用中间件
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// app.use(multer()); // for parsing multipart/form-data
+
+
+//使用模块化路由
 app.use(router);
 // console.log(app);
-
 app.get('/hello', (req, res) => {
     res.send('hello world');
 })
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,63 @@
 const express = require('express');
 
+//mysql driver
+const mysql = require('mysql');
+
 const router = express.Router();
 
-const data = require('../data/students.json');
 
 router.get('/students', (req, res) => {
-    res.send(data);
+
+
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'zr_dev',
+        password: 'YZ4371716',
+        database: 'zr_dev'
+    });
+
+    connection.connect(function (err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+
+        connection.query('SELECT * FROM students', function (err, result) {
+            console.log(err, result);
+
+            res.send(result);
+        })
+
+    });
+
+})
+
+
+router.put('/students', (req, res) => {
+
+    const stu=req.body;
+
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'zr_dev',
+        password: 'YZ4371716',
+        database: 'zr_dev'
+    });
+
+    connection.connect(function (err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+
+        connection.query(`INSERT INTO students (name,gender,age) VALUES ('${stu.name}','${stu.gender}','${stu.age}')`, function (err, result) {
+            console.log(err, result);
+
+            res.send(result);
+        })
+
+    });
+
 })
 
 
