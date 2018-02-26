01274723eab29a50bf2c6693027b1efdfa633d33
zhunrong <chenzr@chd.edu.cn>
2018-02-26 13:57:11 +0800

    '2018/02/26'

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,22 @@ const mysql = require('mysql');
 
 const router = express.Router();
 
-
+//获取学生数据
 router.get('/students', (req, res) => {
 
+    console.log(req.query);
+    const query = req.query;
+
+    //查询条件
+    let sql = 'SELECT * FROM students';
+    let condition = [];
+    for (let key in query) {
+        condition.push(`${key}='${query[key]}'`);
+    }
+    if (condition.length > 0) {
+        sql += ' WHERE ';
+        sql += condition.join(' AND ');
+    }
 
     var connection = mysql.createConnection({
         host: 'localhost',
@@ -16,26 +29,24 @@ router.get('/students', (req, res) => {
         database: 'zr_dev'
     });
 
-    connection.connect(function (err) {
+    connection.query(sql, function (err, result) {
+
         if (err) {
-            console.error('error connecting: ' + err.stack);
+            console.error(err);
             return;
         }
 
-        connection.query('SELECT * FROM students', function (err, result) {
-            console.log(err, result);
+        res.send(result);
+    })
 
-            res.send(result);
-        })
-
-    });
+    connection.end();
 
 })
 
-
+//新增学生数据
 router.put('/students', (req, res) => {
 
-    const stu=req.body;
+    const stu = req.body;
 
     var connection = mysql.createConnection({
         host: 'localhost',
@@ -44,19 +55,86 @@ router.put('/students', (req, res) => {
         database: 'zr_dev'
     });
 
-    connection.connect(function (err) {
+    connection.query(`INSERT INTO students (name,gender,age) VALUES ('${stu.name}','${stu.gender}','${stu.age}')`, function (err, result) {
+        console.log(err, result);
+
+        res.send(result);
+    })
+
+    connection.end();
+
+
+})
+
+//修改学生数据
+router.post('/students', (req, res) => {
+
+    const dataBody = req.body;
+    if (!dataBody.id) {
+        res.send('id不能为空');
+        return;
+    }
+
+    let condition = [];
+    for (let key in dataBody) {
+        if (key === 'id') {
+            continue;
+        }
+        condition.push(`${key}='${dataBody[key]}'`);
+    }
+    let sql = `UPDATE students SET ${condition.join(',')} WHERE id='${dataBody.id}'`;
+
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'zr_dev',
+        password: 'YZ4371716',
+        database: 'zr_dev'
+    });
+
+    connection.query(sql, function (err, result) {
+        console.log(err, result);
         if (err) {
-            console.error('error connecting: ' + err.stack);
+            console.error(err);
             return;
         }
 
-        connection.query(`INSERT INTO students (name,gender,age) VALUES ('${stu.name}','${stu.gender}','${stu.age}')`, function (err, result) {
-            console.log(err, result);
-
-            res.send(result);
-        })
-
+        res.send(result);
     });
+
+    connection.end();
+
+
+})
+
+//删除学生信息
+router.delete('/students', (req, res) => {
+    const dataBody = req.body;
+    if (!dataBody.id) {
+        res.send('id不能为空');
+        return;
+    }
+
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'zr_dev',
+        password: 'YZ4371716',
+        database: 'zr_dev'
+    });
+
+    let sql = `DELETE FROM students WHERE id='${dataBody.id}'`;
+
+
+    connection.query(sql, function (err, result) {
+        console.log(err, result);
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        res.send(result);
+    })
+
+    connection.end();
 
 })
 
