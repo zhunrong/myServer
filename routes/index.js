c4a0f84e97b21e5b6292a5c7601fe5aaa99bbc72
chenzr <chenzr@chd.edu.cn>
2018-03-01 00:00:49 +0800

    进一步拆分路由

--- routes/index.js
+++ routes/index.js
@@ -1,63 +1,8 @@
-const express = require('express');
-const multer = require('multer');
-const router = express.Router();
-const fs = require('fs');
-const path = require('path');
-
-const upload = multer({
-    storage: multer.diskStorage({
-        destination: 'file/uploads',
-        filename: (req, file, cb) => {
-            console.log(file)
-            cb(null, file.originalname);
-        }
-    })
-})
-
-//controller
-const student = require('../controller/student');
-
-//获取学生数据
-router.get('/students', student.get);
-
-//新增学生数据
-router.put('/students', student.put);
-
-//修改学生数据
-router.post('/students', student.post);
-
-//删除学生信息
-router.delete('/students', student.delete);
-
-
-router.post('/upload', upload.array('emoji', 10), (req, res) => {
-
-    res.send(req.files);
-})
-
-router.get('/files', (req, res) => {
-
-    const dir = path.join(__dirname, '../file/uploads')
-    fs.readdir(dir, (err, files) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        console.log(files);
-
-        const result = {};
-        files.forEach(file => {
-            result[file] = `/file/uploads/${file}`;
-        })
-
-        res.send(result);
-
-    })
-
-})
+const student = require('./student');
+const upload = require('./upload');
 
 
 module.exports = app => {
-    app.use(router);
+    app.use(student);
+    app.use(upload);
 }
---
+++ routes/student.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+//controller
+const student = require('../controller/student');
+
+//获取学生数据
+router.get('/students', student.get);
+
+//新增学生数据
+router.put('/students', student.put);
+
+//修改学生数据
+router.post('/students', student.post);
+
+//删除学生信息
+router.delete('/students', student.delete);
+
+
+module.exports = router;
