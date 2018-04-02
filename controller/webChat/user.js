98112d8a52a68cb4517ce1357ab68a852ed68ba8
zhunrong <chenzr@chd.edu.cn>
2018-04-02 17:19:36 +0800

    完善用户修改api

--- controller/webChat/user.js
+++ controller/webChat/user.js
@@ -20,12 +20,49 @@ exports.put = (req, res) => {
 
     const id = req.body.id;
     const nickname = req.body.nickname;
+    const gender = req.body.gender;
+    const age = req.body.age;
+    const province = req.body.province;
+    const city = req.body.city;
+    const say = req.body.say;
+
+    const dataObj = {};
+
+    if (nickname) {
+        dataObj['nickname'] = nickname;
+    }
+    if (gender) {
+        dataObj['gender'] = gender;
+    }
+    if (age) {
+        dataObj['age'] = age;
+    }
+    if (province) {
+        dataObj['province'] = age;
+    }
+    if (city) {
+        dataObj['city'] = city;
+    }
+    if (say) {
+        if (say.length > 100) {
+            res.status(400).send({
+                error: '个性签名长度不能超过100个字符'
+            })
+            return;
+        }
+        dataObj['say'] = say;
+    }
+
     userModel.put({
         id: id
-    }, {
-        nickname: nickname
+    }, dataObj).then(() => {
+        return userModel.get({
+            id: id
+        })
     }).then(result => {
-        res.send(result);
+        res.send({
+            data: result[0]
+        });
     })
 
 }
