309c46aadb949b46b5877b71fc85d7a02eed976d
zhunrong <chenzr@chd.edu.cn>
2018-08-14 19:59:54 +0800

    用户注册

--- routes/common/user.js
+++ routes/common/user.js
@@ -1,4 +1,4 @@
-const router = require('express').Router;
+const router = require('express').Router();
 const userController = require('../../controller/common/user');
 router.post('/user/update', userController.update);
 
